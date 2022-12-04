0207ad75e9a8767eb6cec7e3660bbe2d2089e68b
Максим Луговской <lugovskou.myu@yandex.ru>
2022-12-04 19:44:21 +0500

    add: добавил AboutProject

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,15 @@
 import React from "react";
 // import { Link, useLocation, Switch, Route } from "react-router-dom";
-import Promo from "./Promo/Promo";
+import Promo from "./Promo/Promo.jsx";
+import AboutProject from "./AboutProject/AboutProject.jsx";
 import "./Main.css";
 function Main(props) {
 
   return(
     <section className="Main">
     <Promo />
-    {/* <AboutProject />
-    <Techs />
+    <AboutProject />
+    {/*<Techs />
     <AboutMe /> */}
     </section>
   )
