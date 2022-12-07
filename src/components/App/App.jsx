9070929dec2ac5eb221150d85f3caad49e1a267c
Максим Луговской <lugovskou.myu@yandex.ru>
2022-12-07 23:18:12 +0500

    add: SignIn

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -4,9 +4,10 @@ import React from "react";
 // import Login from "./Login";
 // import Register from "./Register";
 // import Loading from "./Promo";
-import Header from "../Header/Header.jsx";
-import Main from "../Main/Main.jsx";
-import Footer from "../Footer/Footer.jsx";
+// import Header from "../Header/Header.jsx";
+// import Main from "../Main/Main.jsx";
+// import Footer from "../Footer/Footer.jsx";
+import SignIn from "../SignIn/SignIn.jsx";
 // import ProtectedRoute from "./ProtectedRoute";
 import './App.css';
 
@@ -14,9 +15,12 @@ function App() {
   return(
     <div className="App">
       <div className="page">
-        <Header/>
-        <Main />
-        <Footer />
+        {/* <Header/>
+        <Main />  */}
+        <SignIn />
+        {/* <Footer /> */}
+
+
       {/*       <Switch>
             <Route path="/">
               <Loading/>
---
+++ src/components/SignIn/SignIn.jsx
@@ -0,0 +1,75 @@
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import "./SignIn.css";
+import Logo from "../../images/logo.svg";
+
+function SignIn(props) {
+
+  const [email, setEmail ] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleEmailChange(evt) {
+    setEmail(evt.target.value);
+  }
+
+  function handlePasswordChange(evt) {
+    setPassword(evt.target.value);
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    // функция отправки прокинутая пропсом
+    setEmail("");
+    setPassword("");
+  }
+
+  return (
+    <section className="signin">
+      <a className="signin__link-logo" href="/">
+        <img className="signin__logo" alt="Логотип" src={Logo} />
+      </a>
+      <h1 className="signin__title">Рады видеть!</h1>
+      <form onSubmit={handleSubmit} className="signin__form">
+        <p className="signin__lable">E-mail</p>
+        <label htmlFor="email"></label>
+        <input
+          className="signin__input"
+          placeholder="Email"
+          required
+          id="email"
+          name="email"
+          type="email"
+          value={email || ""}
+          onChange={handleEmailChange}
+        />
+        <hr className="signin__line" />
+        <p className="signin__lable">Пароль</p>
+        <label htmlFor="password"></label>
+        <input
+          className="signin__input"
+          placeholder="Пароль"
+          required
+          id="password"
+          name="password"
+          type="password"
+          value={password || ""}
+          onChange={handlePasswordChange}
+        />
+        <hr className="signin__line" />
+        <div className="signin__button-container">
+          <button type="submit" className="signin__save-button">
+            Войти
+          </button>
+        </div>
+        <div className="signin__register-container">
+          <p className="signin__register-title">Ещё не зарегистрированы?</p>
+          <Link className="signin__link" to="/signup">
+            Регистрация
+          </Link>
+        </div>
+      </form>
+    </section>
+  );
+}
+
+export default SignIn;
