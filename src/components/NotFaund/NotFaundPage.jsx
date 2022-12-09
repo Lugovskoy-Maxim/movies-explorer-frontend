7f3fb0356fd7723f7d49d6b86f286eace2c39da9
Максим Луговской <93675697+Lugovskoy-Maxim@users.noreply.github.com>
2022-12-09 16:03:48 +0500

    add: 404 page

---
+++ src/components/NotFaund/NotFaundPage.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { useNavigate } from "react-router-dom";
+import "./NotFaundPage.css";
+
+function NotFaundPage(){
+  const navigate = useNavigate();
+
+  return(
+    <section className="NotFaundPage">
+      <h1 className="notfaund__title">404</h1>
+      <p className="notfaund__description">Страница не найдена</p>
+      <button className="notfaund__button" onClick={() => navigate(-1)}>Назад</button>
+    </section>
+  )
+}
+
+export default NotFaundPage;
