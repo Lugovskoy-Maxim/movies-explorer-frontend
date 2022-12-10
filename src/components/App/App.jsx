b1c8cba37203d1c85cd3576c01d4d6dff4820710
Максим Луговской <lugovskou.myu@yandex.ru>
2022-12-10 15:05:41 +0500

    fix: Исправил отображение аккаунта в шапке

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,56 +1,60 @@
-import React from "react";
-
+// import React from "react";
+import React, { useState } from 'react';
 // import { Route, Switch, useHistory, Redirect } from "react-router-dom";
-
+import { CurrentUserContext } from "../../context/CurrentUserContext";
 // import Login from "./Login";
 // import Register from "./Register";
 // import Loading from "./Promo";
-// import Header from "../Header/Header.jsx";
+import Header from "../Header/Header.js";
 // import Main from "../Main/Main.jsx";
 // import Footer from "../Footer/Footer.jsx";
 // import SignIn from "../SignIn/SignIn.jsx";
 // import SignUp from "../SignUp/SignUp.jsx";
-import NotFaundPage from "../NotFaund/NotFaundPage.jsx";
+// import NotFaundPage from "../NotFaund/NotFaundPage.jsx";
 // import ProtectedRoute from "./ProtectedRoute";
 import './App.css';
 
 function App() {
+  const [currentUser, setCurrentUser] = useState();
   return(
-    <div className="App">
-      <div className="page">
-        {/* <Header/>
-        <Main />
-        <SignIn />
-        <SignUp /> */}
-        <NotFaundPage />
+    <CurrentUserContext.Provider value={currentUser}>
+      <div className="App">
+        <div className="page">
+          <Header
+          onLogin={currentUser}  />
+          {/* <Main />
+          <SignIn />
+          <SignUp />
+          <NotFaundPage /> */}
 
 
-        {/* <Footer /> */}
+          {/* <Footer /> */}
 
 
-      {/*       <Switch>
-            <Route path="/">
-              <Loading/>
-            </Route>
-            <Route path="/signin">
-              <Login/>
-            </Route>
-            <Route path="/signup">
-              <Register/>
-            </Route>
-            <ProtectedRoute
-              exact
-              path="/"
-              component={Main}
-            />
-            <Route path="*">
-              <Redirect to="/404" />
-            </Route>
-          </Switch>
-*/}
+        {/*       <Switch>
+              <Route path="/">
+                <Loading/>
+              </Route>
+              <Route path="/signin">
+                <Login/>
+              </Route>
+              <Route path="/signup">
+                <Register/>
+              </Route>
+              <ProtectedRoute
+                exact
+                path="/"
+                component={Main}
+              />
+              <Route path="*">
+                <Redirect to="/404" />
+              </Route>
+            </Switch>
+  */}
 
+        </div>
       </div>
-    </div>
+    </CurrentUserContext.Provider>
   );
 }
 
---
+++ src/components/Header/Header.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Header.css";
+import Logo from "../../images/logo.svg";
+
+function Header(props) {
+
+  function toggleNavigation() {
+    console.log(props.onLogin)
+    if (!props.onLogin){
+      return(
+        <button className="header__button">Аккаунт</button>
+      )} else {
+        return(
+          <>
+            <Link className="header__link header__link_register" to="/signup">Регистрация</Link>
+            <div className="header__link_login">
+              <Link className="header-link" to="/signin">Войти</Link>
+            </div>
+          </>
+        )
+      }
+    }
+
+    return (
+        <section className="header">
+          <a className="header__link-logo" href="/">
+          <img className="header__logo" alt="Логотип" src={Logo} />
+        </a>
+        <div className="header__navInfo">
+          {toggleNavigation()}
+        </div>
+        </section>
+    );
+  }
+export default Header;
