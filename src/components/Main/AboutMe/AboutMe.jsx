073f8a71b8a441feb1cbca700e51a2179a01ce7b
Максим Луговской <lugovskou.myu@yandex.ru>
2022-12-13 19:46:45 +0500

    Оптимизировал svg и добавил ссылки

--- src/components/Main/AboutMe/AboutMe.jsx
+++ src/components/Main/AboutMe/AboutMe.jsx
@@ -19,14 +19,14 @@ function AboutMe() {
           <h3 className="aboutMe__partfolio_title">Партфолио</h3>
           <ul className="aboutMe__partfolio_links">
             <li className="aboutMe__partfolio_link">
-              <a className="aboutMe__partfolio_project" href="https://github.com/">Страничный сайт</a>
+              <a className="aboutMe__partfolio_project"  target="_blank" href="https://github.com/Lugovskoy-Maxim/react-mesto-api-full">Страничный сайт</a>
               <p className="aboutMe__partfolio_icon">↗</p>
             </li>
           <li className="aboutMe__partfolio_link">
-              <a className="aboutMe__partfolio_project" href="https://github.com/">Адаптивный сайт</a>
+              <a className="aboutMe__partfolio_project"  target="_blank" href="https://github.com/Lugovskoy-Maxim/russian-travel">Адаптивный сайт</a>
               <p className="aboutMe__partfolio_icon">↗</p>
             </li>            <li className="aboutMe__partfolio_link">
-              <a className="aboutMe__partfolio_project" href="https://github.com/">Одностраничное приложение</a>
+              <a className="aboutMe__partfolio_project"  target="_blank" href="https://github.com/Lugovskoy-Maxim/how-to-learn">Одностраничное приложение</a>
               <p className="aboutMe__partfolio_icon">↗</p>
             </li>
           </ul>
