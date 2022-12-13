d178252007efa93a738163628ec57595fc793ea0
Максим Луговской <93675697+Lugovskoy-Maxim@users.noreply.github.com>
2022-12-13 15:06:19 +0500

    add: profile

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,89 @@
+import React, { useState } from "react";
+import { Link, useNavigate  } from "react-router-dom";
+import Header from "../Header/Header.jsx";
+import "./Profile.css";
+import { CurrentUserContext } from "../../context/CurrentUserContext";
+
+function Profile({ signOut}) {
+  const currentUser = React.useContext(CurrentUserContext);
+  const [name, setName ] = useState(currentUser.name);
+  const [email, setEmail] = useState(currentUser.email);
+  const navigate = useNavigate();
+
+  function handleNameChange(evt) {
+    setName(evt.target.value);
+  }
+
+  function handleEmailChange(evt) {
+    setEmail(evt.target.value);
+  }
+
+  function handleSubmit(evt) {
+    evt.preventDefault();
+    // функция отправки лучше прокинуть пропсом
+    setName("");
+    setEmail("");
+  }
+
+  React.useEffect(()=> {
+    checkUser()
+  }, [currentUser])
+
+  function checkUser(){
+    if (!typeof currentUser === 'object'){
+      return navigate("/404")
+    } else {
+      setName(currentUser.name)
+      setEmail(currentUser.email)
+    }
+  }
+
+  return (
+    <>
+    <section className="profile">
+      <h1 className="profile__title">Привет, {name}</h1>
+      <form onSubmit={handleSubmit} className="profile__form">
+        <div className="profile__container">
+          <p className="profile__lable">Имя</p>
+          <label htmlFor="name"></label>
+          <input
+            className="profile__input"
+            placeholder={name}
+            required
+            id="name"
+            name="name"
+            type="name"
+            value={name }
+            onChange={handleNameChange}
+          />
+        </div>
+        <hr className="profile__line" />
+        <div className="profile__container">
+          <p className="profile__lable">E-mail</p>
+          <label htmlFor="email"></label>
+          <input
+            className="profile__input"
+            placeholder={email}
+            required
+            id="email"
+            name="email"
+            type="email"
+            value={email}
+            onChange={handleEmailChange}
+          />
+        </div>
+        <div className="profile__button-container">
+          <button type="submit" className="profile__save-button button">
+            Редактировать
+          </button>
+        </div>
+          <button className="profile__signout-button button" onClick={signOut}>
+            Выйти из аккаунта
+          </button>
+      </form>
+    </section>
+    </>
+  );
+}
+
+export default Profile;
