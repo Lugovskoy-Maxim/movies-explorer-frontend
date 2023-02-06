afc124d8eb42705e0e7027cd0ffaf5fad92032f9
Максим Луговской <lugovskou.myu@yandex.ru>
2023-02-06 09:06:33 +0500

    Испаравил редирект при логауте

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -73,7 +73,7 @@ function Profile({ onSignOut, updateUserInfo }) {
             onClick={
               () => {
                 onSignOut()
-                navigate("/signin")
+                navigate("/")
               }
             }
           >
